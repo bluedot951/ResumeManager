ecffeed8d5d39d34964afb9cf4b1838061194e18
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-07-14 23:13:40 -0700

    Added firebase config

---
+++ web/js/firebase_config.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyBYvNlEjBShvCG4FWK5wHNKSygXCwb2y1A",
+    authDomain: "resumemanager-5c41e.firebaseapp.com",
+    databaseURL: "https://resumemanager-5c41e.firebaseio.com/",
+    storageBucket: "resumemanager-5c41e.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+  };
+  firebase.initializeApp(config);
+</script>

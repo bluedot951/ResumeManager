ad56ee5d6d3c80c1a9b4eae4bb029ede34431a43
Shantanu Gore <SGore-MBP@SGore-MBP.local>
2017-07-14 23:18:11 -0700

    fade animation

--- web/js/script.js
+++ web/js/script.js
@@ -2,14 +2,31 @@ document.onkeydown = checkKey;
 
 function checkKey(e) {
 
-    e = e || window.event;
+	e = e || window.event;
 
-    if (e.keyCode == '37') {
-       // alert('left arrow');
-       document.getElementById("resume").src = "sandeep.jpg";
-    }
-    else if (e.keyCode == '39') {
-       document.getElementById("resume").src = "shantanu.jpg";
-    }
+	if (e.keyCode == '37') {
+       $("#resume").fadeOut(500, function() {
+       	document.getElementById("resume").src = "sandeep.jpg";
+       	$("#resume").fadeIn(500, null);
+       });
+
+	}
+	else if (e.keyCode == '39') {
+       // document.getElementById("resume").src = "shantanu.jpg";
+       // fade();
+       $("#resume").fadeOut(500, function() {
+       	document.getElementById("resume").src = "shantanu.jpg";
+       	$("#resume").fadeIn(500, null);
+       });
+   }
 
 }
+
+// function fade() {
+// 	$("#resume").fadeOut(1000, function() {
+//         document.getElementById("resume").src = "shantanu.jpg";
+//     }).fadeIn(1000);
+//     return false;
+// }
+
+
